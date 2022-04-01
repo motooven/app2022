d1d6d3c2dc3fc2dca0a9395f03ac856821f1a90e
motooven <motooven@gmail.com>
2022-04-01 10:43:12 +0300

    v11

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div>Profile</div>
+            <div>Messages</div>
+            <div>News</div>
+            <div>Settings</div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <div>Avatarka</div>
+            <div>My post</div>
+            <div>post 1</div>
+        </div>
+    );
+};
+
+export default Profile;
