1afc12a2f53b73c9718f8cb3c46356f25eecdfda
motooven <motooven@gmail.com>
2022-04-01 13:28:12 +0300

    v21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className="dialogs">
-            Dialogs
+            <div>
+                <div><NavLink to="/dialogs/1">Oleg</NavLink></div>
+                <div><NavLink to="/dialogs/2">Jeka</NavLink></div>
+                <div><NavLink to="/dialogs/3">Ivan</NavLink></div>
+
+            </div>
+            <div>
+                <div>Eee</div>
+                <div>Eeeeee</div>
+                <div>Eeeeeeeeee</div>
+            </div>
         </div>
     );
 };
